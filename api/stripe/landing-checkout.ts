e92fec1c8750da0a3487e0bee105428a4eaa487b
Ujjwal <garry@123>
2026-03-07 12:39:13 +0530

    build

--- api/stripe/landing-checkout.ts
+++ api/stripe/landing-checkout.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-01-28.clover',
+  apiVersion: '2026-02-28.clover',
 });
 
 // Package configuration with descriptions
